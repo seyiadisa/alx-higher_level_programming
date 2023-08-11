dac0acb6b99e11c5688c96e936ad1035ad3d2d4a
Oluwaseyi Adisa <oluwaseyiadisaa@gmail.com>
2023-08-11 05:55:51 +0100

    fix checker errors for python - if/else

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def print_last_digit(number):
     last_digit = int(str(number)[-1])
-    print(last_digit)
+    print(last_digit, end="")
 
     return last_digit
+print_last_digit(98)
