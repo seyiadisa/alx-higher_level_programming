6757156fcbbf3d36749da4184683ccf6d9702257
Oluwaseyi Adisa <oluwaseyiadisaa@gmail.com>
2023-08-29 00:06:59 +0100

    fix error in 1-safe_print_integer.py

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -2,7 +2,6 @@
 def safe_print_integer(value):
     try:
         print("{:d}".format(int(value)))
-    except ValueError:
+        return True
+    except Exception:
         return False
-
-    return True
