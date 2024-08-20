5a5fa0210c3a651cb365414aeb719f8b8d399625
Oluwaseyi Adisa <oluwaseyiadisaa@gmail.com>
2024-08-20 01:46:23 +0100

    fix errors for task 10-factorial

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -2,9 +2,8 @@
 const { argv } = require('node:process');
 
 function factorial (n) {
-  if (n === 0) {
-    return 1;
-  }
+  if (isNaN(n)) return 1;
+  if (n === 0) return 1;
 
   return n * factorial(n - 1);
 }
